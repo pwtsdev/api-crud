52d338a1b64b06dea5e9811d13b97b6a822870b2
bkita <2436425+bkita@users.noreply.github.com>
2025-03-09 10:33:42 +0100

    test: response details - part II

--- tests/instructors/instructors.response.spec.ts
+++ tests/instructors/instructors.response.spec.ts
@@ -1,11 +1,48 @@
 import test, { expect } from '@playwright/test';
 
+// APIResponse
+// APIResponse class represents responses returned by apiRequestContext.get() and similar methods.
+// https://playwright.dev/docs/api/class-apiresponse#api-response-headers
+
 test('APIResponse details', async ({ request }) => {
   const response = await request.get('http://localhost:3000/instructors/vflr');
   expect(response.status()).toBe(200);
 
+  // body
   console.log('👉 response body');
   console.log(await response.body());
-  console.log((await response.body()).toString());
-  console.log(JSON.parse((await response.body()).toString()));
+  // console.log((await response.body()).toString());
+  // console.log(JSON.parse((await response.body()).toString()));
+
+  // text
+  console.log('👉 response body text');
+  console.log(await response.text());
+
+  // json
+  console.log('👉 response body json');
+  console.log(await response.json());
+
+  // status
+  console.log('👉 response status');
+  console.log(response.status());
+
+  // status text
+  console.log('👉 response status text');
+  console.log(response.statusText());
+
+  // ok - status in the range 200-299
+  console.log('👉 response ok');
+  console.log(response.ok());
+
+  // headers
+  console.log('👉 response headers');
+  console.log(response.headers());
+
+  // headersArray
+  console.log('👉 response headersArray');
+  console.log(response.headersArray());
+
+  // url
+  console.log('👉 response url');
+  console.log(response.url());
 });
