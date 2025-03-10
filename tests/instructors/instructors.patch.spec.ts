838e7be7e3268b5b4ac225d705aa49431b1f75cd
bkita <2436425+bkita@users.noreply.github.com>
2025-03-10 10:10:18 +0100

    test: PATCH instructors

---
+++ tests/instructors/instructors.patch.spec.ts
@@ -0,0 +1,43 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { getRandomInstructorData } from '@datafactory/instructor.data';
+import { expect, test } from '@fixtures/api.fixture';
+import { createNewInstructorRequest } from '@requests/instructors/create.instructor.request';
+import { readInstructorByIdRequest } from '@requests/instructors/read.instructor.request';
+
+test.describe('PATCH /instructors/:id', () => {
+  test('should create and update an instructor successfully', async ({ request }) => {
+    let instructorId: string;
+
+    const newInstructor = getRandomInstructorData();
+
+    const updatedInstructor = {
+      name: 'Joe Black',
+    };
+
+    await test.step('Create a new instructor', async () => {
+      const responseData = await (await createNewInstructorRequest(newInstructor)).json();
+      instructorId = responseData.id;
+    });
+
+    await test.step('Partially update the instructor', async () => {
+      const updateResponse = await request.patch(`/instructors/${instructorId}`, {
+        data: updatedInstructor,
+      });
+
+      expect(updateResponse.status()).toBe(200);
+
+      const responseData = await updateResponse.json();
+      expect(responseData.name).toBe(updatedInstructor.name);
+      expect(responseData.email).toBe(newInstructor.email);
+    });
+
+    await test.step('Verify the updated instructor', async () => {
+      const responseData = await (await readInstructorByIdRequest(instructorId)).json();
+      expect(responseData.name).toBe(updatedInstructor.name);
+      expect(responseData.email).toBe(newInstructor.email);
+    });
+  });
+});
